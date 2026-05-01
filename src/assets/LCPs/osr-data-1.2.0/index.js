26a76aa481d00995afce5a5a70b4f2e13bfeb711
jackhoustongm-code <jackhouston.gm@gmail.com>
2026-05-02 00:31:28 +0100

    Add index.js to export game data modules

---
+++ src/assets/LCPs/osr-data-1.2.0/index.js
@@ -0,0 +1,16 @@
+"use strict"
+import f from "./frames.json"
+import s from "./systems.json"
+import w from "./weapons.json"
+import t from "./talents.json"
+import r from "./reserves.json"
+
+const data = {
+  frames: f,
+  reserves: r,
+  systems: s,
+  weapons: w,
+  talents: t,
+}
+
+export default data;
