db4e52e5a07b8ef3890ac255e730c01ef3d2775c
jackhoustongm-code <jackhouston.gm@gmail.com>
2026-05-03 05:56:45 +0100

    Add index.js to aggregate JSON data imports

---
+++ src/assets/LCPs/osr-data-1.2.0/index.js
@@ -0,0 +1,16 @@
+"use strict"
+import f from "./frames.json"
+import r from "./reserves.json"
+import s from "./systems.json"
+import t from "./talents.json"
+import w from "./weapons.json"
+
+const data = {
+  frames: f,
+  reserves: r,
+  systems: s,
+  talents: t,
+  weapons: w,
+}
+
+export default data;
